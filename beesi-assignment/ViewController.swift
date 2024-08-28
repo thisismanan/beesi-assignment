b92ac1dc548f4230132f911ce889d9ad7237bc7c
Manan <bhatiamanan4@gmail.com>
2024-08-28 21:18:37 +0530

    Initial Commit

---
+++ beesi-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beesi-assignment
+//
+//  Created by Manan Bhatia on 28/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
